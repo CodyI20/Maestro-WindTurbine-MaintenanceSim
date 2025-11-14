b4297629af94f1e0b88a67588d3e7de0c9595c36
Cody <codrin.ilie10@gmail.com>
2025-11-14 12:31:56 +0100

    Issue #116 - Restore to default
    Added functionality to the "Restore to default" button in the settings menu

---
+++ MaestroSimulation/Source/MaestroSimulation/Private/SettingsComparisonBPLibrary.cpp
@@ -0,0 +1,73 @@
+// SettingsComparisonBPLibrary.cpp
+#include "SettingsComparisonBPLibrary.h"
+#include "UObject/UnrealType.h"
+#include "GameFramework/SaveGame.h"
+
+
+FProperty* USettingsComparisonBPLibrary::FindPropertyByName(USaveGame* SaveGameObject, const FName& PropertyName)
+{
+    if (!SaveGameObject)
+        return nullptr;
+
+    for (TFieldIterator<FProperty> PropIt(SaveGameObject->GetClass()); PropIt; ++PropIt)
+    {
+        FProperty* Property = *PropIt;
+        if (Property->GetFName() == PropertyName)
+        {
+            return Property;
+        }
+    }
+    return nullptr;
+}
+
+bool USettingsComparisonBPLibrary::HasFloatChanged(USaveGame* SaveGameObject, FName PropertyName, float CurrentValue, float Tolerance)
+{
+    FProperty* Property = FindPropertyByName(SaveGameObject, PropertyName);
+    if (FStructProperty* StructProp = CastField<FStructProperty>(Property))
+    {
+        // Could add struct type-specific handling here (e.g. FVector)
+    }
+    if (FFloatProperty* FloatProp = CastField<FFloatProperty>(Property))
+    {
+        const void* ValuePtr = FloatProp->ContainerPtrToValuePtr<void>(SaveGameObject);
+        float SavedValue = FloatProp->GetFloatingPointPropertyValue(ValuePtr);
+        return !FMath::IsNearlyEqual(SavedValue, CurrentValue, Tolerance);
+    }
+    return true; // If property not found or type mismatch, assume changed
+}
+
+bool USettingsComparisonBPLibrary::HasBoolChanged(USaveGame* SaveGameObject, FName PropertyName, bool CurrentValue)
+{
+    FProperty* Property = FindPropertyByName(SaveGameObject, PropertyName);
+    if (FBoolProperty* BoolProp = CastField<FBoolProperty>(Property))
+    {
+        const void* ValuePtr = BoolProp->ContainerPtrToValuePtr<void>(SaveGameObject);
+        bool SavedValue = BoolProp->GetPropertyValue(ValuePtr);
+        return SavedValue != CurrentValue;
+    }
+    return true;
+}
+
+bool USettingsComparisonBPLibrary::HasStringChanged(USaveGame* SaveGameObject, FName PropertyName, const FString& CurrentValue)
+{
+    FProperty* Property = FindPropertyByName(SaveGameObject, PropertyName);
+    if (FStrProperty* StrProp = CastField<FStrProperty>(Property))
+    {
+        const void* ValuePtr = StrProp->ContainerPtrToValuePtr<void>(SaveGameObject);
+        FString SavedValue = StrProp->GetPropertyValue(ValuePtr);
+        return !SavedValue.Equals(CurrentValue, ESearchCase::CaseSensitive);
+    }
+    return true;
+}
+
+bool USettingsComparisonBPLibrary::HasIntChanged(USaveGame* SaveGameObject, FName PropertyName, int32 CurrentValue)
+{
+    FProperty* Property = FindPropertyByName(SaveGameObject, PropertyName);
+    if (FIntProperty* IntProp = CastField<FIntProperty>(Property))
+    {
+        const void* ValuePtr = IntProp->ContainerPtrToValuePtr<void>(SaveGameObject);
+        int32 SavedValue = IntProp->GetSignedIntPropertyValue(ValuePtr);
+        return SavedValue != CurrentValue;
+    }
+    return true;
+}
